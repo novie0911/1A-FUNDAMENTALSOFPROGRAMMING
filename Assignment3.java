b84466be4f33f47e6b7443e5cf83dee249b4a884
Assignment3.java <26ur0163@psu.edu.ph>
2026-09-01 21:00:40 +0800

    assignment

---
+++ Assignment3.java
@@ -0,0 +1,21 @@
+public class Assignment3 {
+
+public static void main(String[] args){
+
+        String firstname = "Novie Joy";
+        String lastname = "Reglos";
+        String address = "Binalonan";
+        String mobileNumber = "09623846604";
+        int age = 18;
+        double gwa = 93.44;
+
+        System.out.println("firstname: " + firstname);
+        System.out.println("lastname: " + lastname);
+        System.out.println("address: " + address);
+        System.out.println("mobileNumber: " + mobileNumber);
+        System.out.println("gwa: " + gwa);
+
+
+
+}
+}
